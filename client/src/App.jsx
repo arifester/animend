ab2e1b08fc5a0433c7f8b1ad22e737e9daece93d
Arifester <trainersterarif@gmail.com>
2026-01-14 17:07:24 +0700

    feat: add Search route to enable anime search functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "@/components/Navbar"
 import Home from "@/pages/Home"
 import AnimeDetail from "@/pages/AnimeDetail"
+import Search from "@/pages/Search"
 
 function App() {
   const currentYear = new Date().getFullYear();
@@ -15,6 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         
         <Route path="/anime/:id" element={<AnimeDetail />} />
+
+        <Route path="/search/:query" element={<Search />} />
       </Routes>
       
       <footer className="py-8 text-center border-t border-slate-900 mt-auto">
