dc4506ae07df38bc47959fd0f5cd09b0a820b575
Arifester <trainersterarif@gmail.com>
2026-01-26 20:38:37 +0700

    feat: add route for user recommendations in animeRoutes

--- server/src/routes/animeRoutes.js
+++ server/src/routes/animeRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware');
-const { addToWishlist, getWishlist, removeFromWishlist } = require('../controllers/animeController');
+const { addToWishlist, getWishlist, removeFromWishlist, getUserRecommendations } = require('../controllers/animeController');
 
 // Protected Routes: User must be logged in
 router.post('/wishlist', verifyToken, addToWishlist);
 router.get('/wishlist', verifyToken, getWishlist);
 router.delete('/wishlist/:animeId', verifyToken, removeFromWishlist);
+router.get('/recommendations', verifyToken, getUserRecommendations);
 
 module.exports = router;
